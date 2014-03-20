99d6d3bd9bfeb132729f970798827628d3ccabb0
Katherine Lee <kate.lee168@gmail.com>
2014-03-20 14:44:51 -0400

    Made some changes to the benchmark to reflect dataset

--- KaggleDigit/rf_benchmark.R
+++ KaggleDigit/rf_benchmark.R
@@ -2,13 +2,15 @@
 
 library(randomForest)
 
-train <- read.csv("../data/train.csv", header=TRUE)
-test <- read.csv("../data/test.csv", header=TRUE)
+train <- read.csv("/data/train.csv", header=TRUE)
+test <- read.csv("/data/test.csv", header=TRUE)
 
 labels <- as.factor(train[,1])
 train <- train[,-1]
 
+begTime <- Sys.time()  
 rf <- randomForest(train, labels, xtest=test, ntree=1000)
+runTime <- Sys.time()-begTime
 predictions <- levels(labels)[rf$test$predicted]
 
 write(predictions, file="rf_benchmark.csv", ncolumns=1) 
